23407ea28cd83696d1aa52321e6d1ec6ba56b041
alanbacay02 <alan.social02@gmail.com>
2023-04-06 14:14:25 +0800

    adds basic elements, quote generation, and API calls

--- src/App.js
+++ src/App.js
@@ -1,29 +1,74 @@
 import './App.css';
-import quotes from './quotes.json';
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
-function QuoteGenerator() {
+function QuoteDisplay( {quote} ) {
 	return (
 		<div>
-			<p>{quotes[0].quote}{quotes[0].author}</p>
+			<p>{quote.quote}</p>
+			<p> - {quote.author}</p>
 		</div>
 	);
 }
+QuoteDisplay.propTypes = {
+	quote: PropTypes.object.isRequired
+};
 
-function QuoteBox() {
+function QuoteBox( {handleNewQuoteClick, currentQuote} ) {
 	return (
 		<div>
-			<QuoteGenerator />
-			<button>Generate Quote</button>
+			<QuoteDisplay quote={currentQuote} />
+			<button onClick={handleNewQuoteClick}>Generate Quote</button>
 			<button>facebook button</button>
 			<button>twitter button</button>
 		</div>
 	);
 }
+QuoteBox.propTypes = {
+	handleNewQuoteClick: PropTypes.func.isRequired,
+	currentQuote: PropTypes.object.isRequired
+};
 
 export default function App() {
+	// Creates state to keep track of current quote.
+	const [currentQuoteAuthor, setCurrentQuoteAuthor] = useState('');
+	
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const response = await fetch('./quotes.json');
+				const quoteData = await response.json();
+				setCurrentQuoteAuthor(quoteData[Math.floor(Math.random() * quoteData.length)]);
+			} catch (error) {
+				console.log('Error retrieving quotes:', error);
+				setCurrentQuoteAuthor({quote: 'Uh oh! An error occured while retrieving quotes. Please try again later!', author: ''});
+			}
+		}
+		fetchData();
+	}, []);
+
+	function handleNewQuoteClick() {
+		async function fetchDataOnClick() {
+			try {
+				const response = await fetch('./quotes.json');
+				const newQuoteData = await response.json();
+				let newQuote = newQuoteData[Math.floor(Math.random() * newQuoteData.length)];
+				while (newQuote.quote == currentQuoteAuthor.quote) {
+					newQuote = newQuoteData[Math.floor(Math.random() * newQuoteData.length)];
+				}
+				setCurrentQuoteAuthor(newQuote);
+			} catch (error) {
+				console.log('Error retrieving quotes:', error);
+				setCurrentQuoteAuthor({quote: 'Uh oh! An error occured while retrieving quotes. Please try again later!', author: ''});
+			}
+		}
+		fetchDataOnClick();
+	}
+	
 	return (
 		<div className="App">
-			<QuoteBox />
+			{/* Returns a child element <QuoteBox /> where function `handleNewQuoteClick` and `currentQuote` is passed in as props */}
+			<QuoteBox handleNewQuoteClick={handleNewQuoteClick} currentQuote={currentQuoteAuthor}/>
 		</div>
 	);
 }
